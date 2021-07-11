f60f70e1837117c7681f9117b6e78879709e7c03
Ben Zweber <codemunkie@gmail.com>
2021-07-11 13:52:41 -0500

    add image binary request and display response

--- app/src/main/java/com/timothyolt/evolutionarydesign/MainActivity.kt
+++ app/src/main/java/com/timothyolt/evolutionarydesign/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.timothyolt.evolutionarydesign
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.BufferedInputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            val result = getImage()
+
+            val image = BitmapFactory.decodeByteArray(result, 0, result.size)
+
+            findViewById<ImageView>(R.id.image)
+                .setImageBitmap(image)
+        }
+    }
+
+    private suspend fun getImage(): ByteArray = withContext(Dispatchers.IO) {
+        val connection = URL("https://i.imgur.com/GSwTJrM.jpeg")
+            .let { it.openConnection() as HttpURLConnection }
+            .apply { requestMethod = "GET" }
+
+        val inputStream = BufferedInputStream(connection.inputStream)
+
+        val data = ByteArray(connection.contentLength)
+        var offset = 0
+
+        while (offset < connection.contentLength) {
+            inputStream.read(data, offset, data.size - offset)
+                .takeIf { numBytes -> numBytes != -1 }
+                ?.also { numBytes -> offset += numBytes }
+                ?: break
+
+        }
+
+        inputStream.close()
+
+        connection.disconnect()
+
+        data
     }
 }
