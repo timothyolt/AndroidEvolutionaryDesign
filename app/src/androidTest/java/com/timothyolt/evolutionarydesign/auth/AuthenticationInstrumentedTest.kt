a94a06d68a442e94e1f9023daa016549c861c896
Tim Oltjenbruns <tim.oltjenbruns@softvision.com>
2021-07-11 17:35:46 -0400

    Launch Authentication Activity with callback Uri

--- app/src/androidTest/java/com/timothyolt/evolutionarydesign/auth/AuthenticationInstrumentedTest.kt
+++ app/src/androidTest/java/com/timothyolt/evolutionarydesign/auth/AuthenticationInstrumentedTest.kt
@@ -1,5 +1,8 @@
 package com.timothyolt.evolutionarydesign.auth
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -10,7 +13,13 @@ import kotlin.test.*
 class AuthenticationInstrumentedTest {
     @Test
     fun startAuthentication() {
-        val activity = launchActivity<AuthenticationActivity>()
+        val activity = launchActivity<Activity>(
+            Intent(
+                Intent.ACTION_VIEW
+            ).apply {
+                data = Uri.parse("https://timothyolt.com/android-evolutionary-design/login")
+            }
+        )
         activity.moveToState(Lifecycle.State.DESTROYED)
     }
 }
