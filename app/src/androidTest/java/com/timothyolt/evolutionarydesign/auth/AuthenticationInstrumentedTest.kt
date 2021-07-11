816be1d8347d108814044996868b636dd20a9856
Tim Oltjenbruns <tim.oltjenbruns@softvision.com>
2021-07-11 18:41:59 -0400

    Two basic steps of OAuth

--- app/src/androidTest/java/com/timothyolt/evolutionarydesign/auth/AuthenticationInstrumentedTest.kt
+++ app/src/androidTest/java/com/timothyolt/evolutionarydesign/auth/AuthenticationInstrumentedTest.kt
@@ -5,21 +5,45 @@ import android.content.Intent
 import android.net.Uri
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.not
 import org.junit.runner.RunWith
 import kotlin.test.*
+
 
 @RunWith(AndroidJUnit4::class)
 class AuthenticationInstrumentedTest {
     @Test
-    fun startAuthentication() {
-        val activity = launchActivity<Activity>(
+    fun resumeAuthentication() {
+        val scenario = launchActivity<Activity>(
             Intent(
                 Intent.ACTION_VIEW
             ).apply {
-                data = Uri.parse("https://timothyolt.com/android-evolutionary-design/login")
+                data = Uri.parse("http://timothyolt.com/android-evolutionary-design/login#access_token=test")
             }
         )
+        // todo: test toast?
+//        scenario.onActivity { activity ->
+//            onView(
+//                withText("test")
+//            ).inRoot(
+//                withDecorView(not(activity.window.decorView))
+//            ).check(
+//                matches(isDisplayed())
+//            )
+//        }
+        assertEquals(Lifecycle.State.DESTROYED, scenario.state)
+        // todo: assert login state set
+    }
+
+    @Test
+    fun startAuthentication() {
+        val activity = launchActivity<AuthenticationActivity>()
         activity.moveToState(Lifecycle.State.DESTROYED)
     }
 }
--- app/src/main/java/com/timothyolt/evolutionarydesign/auth/AuthenticationActivity.kt
+++ app/src/main/java/com/timothyolt/evolutionarydesign/auth/AuthenticationActivity.kt
@@ -1,12 +1,53 @@
 package com.timothyolt.evolutionarydesign.auth
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(LinearLayout(this))
+        val authentication = authentication(intent)
+        if (authentication == null) {
+            val outgoingOAuthIntent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://api.imgur.com/oauth2/authorize?client_id=6b1112a4f9783ad&response_type=token")
+            }
+            startActivity(outgoingOAuthIntent)
+        } else {
+            Toast.makeText(this, authentication.accessToken, Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
+
+    private fun authentication(intent: Intent): Authentication? {
+        val uri = intent.data
+        return if (
+            uri != null
+            && uri.host == "timothyolt.com"
+            && uri.path == "/android-evolutionary-design/login"
+        ) {
+            val fragments = uri.fragment?.split('&')?.associate {
+                val (first, second) = it.split('=')
+                first to second
+            } ?: emptyMap()
+            val accessToken = fragments["access_token"]
+            if (accessToken != null) {
+                Authentication(
+                    accessToken = accessToken,
+                    tokenType = fragments["token_type"],
+                    expiresIn = fragments["expires_in"]
+                )
+            } else null
+        } else null
+    }
+
+    data class Authentication(
+        val accessToken: String,
+        val tokenType: String?,
+        val expiresIn: String?
+    )
 }
