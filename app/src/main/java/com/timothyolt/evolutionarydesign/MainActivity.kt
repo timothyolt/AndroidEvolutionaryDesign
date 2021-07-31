fccb9ac24ad4380e29c791e9eb51ceb22966ef80
Ben Zweber <codemunkie@gmail.com>
2021-07-30 21:58:23 -0500

    add image retrieval for upload

--- app/src/main/java/com/timothyolt/evolutionarydesign/MainActivity.kt
+++ app/src/main/java/com/timothyolt/evolutionarydesign/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.timothyolt.evolutionarydesign
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.BufferedInputStream
@@ -13,6 +20,22 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    private val pickImage = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri ->
+        val bitmap = when (Build.VERSION.SDK_INT) {
+            in 0..27 -> {
+                MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            }
+            else -> {
+                ImageDecoder.createSource(contentResolver, uri)
+                    .let { source -> ImageDecoder.decodeBitmap(source) }
+            }
+        }
+
+        uploadImage(bitmap)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,8 +45,11 @@ class MainActivity : AppCompatActivity() {
 
             val image = BitmapFactory.decodeByteArray(result, 0, result.size)
 
-            findViewById<ImageView>(R.id.image)
-                .setImageBitmap(image)
+            findViewById<ImageView>(R.id.image).setImageBitmap(image)
+        }
+
+        findViewById<Button>(R.id.uploadButton).setOnClickListener {
+            pickImage.launch("image/*")
         }
     }
 
@@ -39,5 +65,14 @@ class MainActivity : AppCompatActivity() {
         connection.disconnect()
 
         data
+    }
+
+    /**
+     * Upload an image to the currently authenticated Imgur account.
+     *
+     * @return A reference to the upload job.
+     */
+    private fun uploadImage(image: Bitmap) = GlobalScope.launch {
+
     }
 }
