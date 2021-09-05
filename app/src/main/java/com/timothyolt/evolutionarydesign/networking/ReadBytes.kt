8e03787d4798508780ec23a0e2f6457fafae827b
Ben Zweber <codemunkie@gmail.com>
2021-09-05 12:34:48 -0700

    add missing connection and asUrl extensions

--- app/src/main/java/com/timothyolt/evolutionarydesign/networking/ReadBytes.kt
+++ app/src/main/java/com/timothyolt/evolutionarydesign/networking/ReadBytes.kt
@@ -2,6 +2,8 @@ package com.timothyolt.evolutionarydesign.networking
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.net.HttpURLConnection
+import java.net.URL
 import java.net.URLConnection
 
 @Suppress("BlockingMethodInNonBlockingContext")
@@ -9,4 +11,12 @@ suspend fun URLConnection.readBytes(): ByteArray = withContext(Dispatchers.IO) {
     getInputStream()
         .buffered()
         .use { it.readBytes() }
+}
+
+fun String.asUrl(): URL {
+    return URL(this)
+}
+
+suspend fun <T> URL.connection(block: suspend URLConnection.() -> T): T {
+    return (openConnection() as HttpURLConnection).block()
 }
