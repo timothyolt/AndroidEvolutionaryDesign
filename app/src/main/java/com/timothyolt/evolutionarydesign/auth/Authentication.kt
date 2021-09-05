f982cdc9f64b51931ce36c15e3e03cdc8a84673a
Ben Zweber <codemunkie@gmail.com>
2021-09-05 12:33:31 -0700

    add Authentication model for injection

---
+++ app/src/main/java/com/timothyolt/evolutionarydesign/auth/Authentication.kt
@@ -0,0 +1,11 @@
+package com.timothyolt.evolutionarydesign.auth
+
+class Authentication {
+    var state: State? = null
+
+    data class State(
+        val accessToken: String,
+        val tokenType: String?,
+        val expiresIn: String?
+    )
+}
